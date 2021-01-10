e11dd6189c59f66d96c645bfdd09287824f102e7
aashish-pandey <62618382+aashish-pandey@users.noreply.github.com>
2021-01-10 22:07:08 +0530

    Add files via upload

---
+++ src/levels.js
@@ -0,0 +1,40 @@
+import Brick from "./brick";
+
+export function buildLevel(game, level) {
+  let bricks = [];
+
+  level.forEach((row, rowIndex) => {
+    row.forEach((brick, brickIndex) => {
+      let position = {
+        x: 80 * brickIndex,
+        y: 75 + 24 * rowIndex
+      };
+
+      if (brick === 1) {
+        bricks.push(new Brick(game, position));
+      }
+    });
+  });
+
+  return bricks;
+}
+
+export const level1 = [
+  // [1, 1, 0, 0, 1, 0, 1, 0, 1, 1],
+
+  // [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+
+  // [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+
+  [0, 0, 0, 0, 0, 0, 0, 0, 1, 0]
+];
+
+export const level2 = [
+  [1, 1, 0, 1, 1, 0, 1, 1, 1, 1],
+
+  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+
+  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+
+  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+];
